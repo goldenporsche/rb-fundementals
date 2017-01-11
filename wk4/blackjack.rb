9ae6442e1677d046fc048beacd4b1b29e2afdbb2
goldenporsche <katiebaldesweiler@gmail.com>
2017-01-11 00:33:35 -0500

    Blackjack Work
    wk4

--- wk4/blackjack.rb
+++ wk4/blackjack.rb
@@ -1,41 +1,81 @@
 class Player
-  def initialize(cards)
-    @cards = cards    
-  end 
+  def initialize
+    @hand = []
+  end
+
+  def deal(card)
+    @hand.push(card)
+  end
+
+  def hand_value
+    sum = 0
+    @hand.each do |x|
+      sum = sum + x
+    end
+  end
+
+  def new_hand
+    @hand.clear
+  end
 end
 
 class Dealer
-  def initialize(cards)
-    @cards = cards    
+  def initialize
+    @hand = []
+  end
+
+  def deal(card)
+    @hand.push(card)
+  end
+
+  def hand_value
+    sum = 0
+    @hand.each do |x|
+      sum = sum + x
+    end
+  end
+
+  def new_hand
+    @hand.clear
   end
 end
 
-class Game
+class Blackjack
+  def initialize
+    @player = Player.new
+    @dealer = Dealer.new
+    @deck = [1,2,3,4,5,6,7,8,9,10,10,10,10] * 4
+    @deck.shuffle!
+  end
+
   def deal
-    cards
-    2.times { cards << rand(1..10) }
-    cards
+    2.times do
+      @player.deal(@deck.next)
+      @dealer.deal(@deck.next)
+    end
   end
-end
 
-def play_blackjack(deal)
+  def hit
+      @player.deal(@deck.next)
+      @dealer.deal(@deck.next)
+  end
 
-dealers_cards = Array.new
-players_cards = Array.new
+  def new_game
+    @player.new_hand
+    @dealer.new_hand
+    @deck = [1,2,3,4,5,6,7,8,9,10,10,10,10] * 4
+    @deck.shuffle!
+  end
+end  
 
-"dealers_card_1" = rand(1..10)
-"dealers_card_2" = rand(1..10)
-dealers_cards = ["dealers_card_1", "dealers_card_2"]
-puts "dealers_card_1"
-dealers_hand = "dealers_card_1" + "dealers_card_2"
 
 if dealers_hand == 21
-  then puts "Sorry, you lose. The dealer has 21."
+then puts "Sorry, you lose. The dealer has 21."
 
 if dealers_hand == 17
-  then puts "The dealer has 17. Dealer holds."
+then puts "The dealer has 17. Dealer holds."
 
 if dealers_hand > 21
-  then puts "The dealers hand is over 21, the dealer folds."
+then puts "The dealers hand is over 21, the dealer busts."
 
 
